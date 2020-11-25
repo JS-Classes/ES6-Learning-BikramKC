42a632eea46596cc604058b6746343eb6e689186
kc-bikram <bikram.kc@cotiviti.com>
2020-11-25 13:01:51 +0545

    Variable declaration

---
+++ VariableDeclarations.js
@@ -0,0 +1,23 @@
+// Learning Datatypes
+var x;
+x = 45.0980;
+console.log(x);
+x = "dkkdfd";
+console.log(x);
+x = true;
+console.log(x);
+var d = 2n ** 200n
+console.log(d);
+var e = undefined;
+console.log(e);
+var f = null;
+console.log(f);
+
+//Structural
+var g = [2, 3, 4, 5];
+console.log(g);
+var h = {fname: "Bikram", lname: "KC"};
+console.log(h);
+
+
+
