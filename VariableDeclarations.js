6bd9bfdd4ec6a6fc01d5b84354c9f99404dfdba6
kc-bikram <bikram.kc@cotiviti.com>
2020-11-26 09:20:05 +0545

    Datatypes and typeof

--- VariableDeclarations.js
+++ VariableDeclarations.js
@@ -12,12 +12,25 @@ var e = undefined;
 console.log(e);
 var f = null;
 console.log(f);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(+Infinity);
+var c1 = NaN;
+c1 = "djfdj"/23;
+console.log(c1);
 
 //Structural
 var g = [2, 3, 4, 5];
 console.log(g);
 var h = {fname: "Bikram", lname: "KC"};
 console.log(h);
+
+console.log(typeof(h));
+console.log(typeof(x));
+console.log(typeof(f));
+console.log(typeof(g));
+
+
 
 
 
